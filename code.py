c8029df833c05a88733dabb356530443ae8d2945
Rutvij-1 <menavlikarrutvij@gmail.com>
2021-09-29 15:58:25 +0530

    Attempt to debug

--- code.py
+++ code.py
@@ -3,7 +3,7 @@ import sys
 import discord
 from dotenv import load_dotenv
 if True:
-    sys.path.insert('Codeforces-Practice-Ladders')
+    sys.path.append('Codeforces-Practice-Ladders')
     from personalised import *
     from script import *
 
